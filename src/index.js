0112cefc64f330b8d92897e02f235a064362e46b
Enivia <aqingcc@outlook.com>
2022-02-10 22:03:27 +0800

    reconciliation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const TEXT_ELEMENT = "TEXT_ELEMENT";
-
 let nextUnitOfWork = null;
 let wipRoot = null;
+let currentRoot = null;
+let deletions = null;
 
 function createTextElement(text) {
   return {
-    type: TEXT_ELEMENT,
+    type: "TEXT_ELEMENT",
     props: {
       nodeValue: text,
       children: [],
@@ -24,8 +24,51 @@ function createElement(type, props, ...children) {
   };
 }
 
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) => key !== "children" && !isEvent(key);
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
+
+function updateDom(dom, prevProps, nextProps) {
+  //Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = "";
+    });
+
+  // Set new or changed properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
 function commitRoot() {
+  deletions.forEach(commitWork);
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
 }
 
@@ -33,7 +76,15 @@ function commitWork(fiber) {
   if (!fiber) {
     return;
   }
-  fiber.parent.dom.appendChild(fiber.dom);
+  const parentDom = fiber.parent.dom;
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom !== null) {
+    parentDom.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    parentDom.removeChild(fiber.dom);
+  }
+
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
@@ -44,20 +95,18 @@ function render(element, container) {
     props: {
       children: [element],
     },
+    alternate: currentRoot, // old fiber
   };
+  deletions = [];
   nextUnitOfWork = wipRoot;
 }
 
 function createDom(fiber) {
   const dom =
-    fiber.type == TEXT_ELEMENT
+    fiber.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(fiber.type);
-  const { children, ...props } = fiber.props;
-  // assign props
-  Object.keys(props).forEach((name) => {
-    dom[name] = props[name];
-  });
+  updateDom(dom, {}, fiber.props);
   return dom;
 }
 
@@ -68,21 +117,7 @@ function performUnitOfWork(fiber) {
 
   // create fibers for children
   const children = fiber.props.children;
-  let prevSibling = null;
-  children.forEach((child, index) => {
-    const currentFiber = {
-      type: child.type,
-      props: child.props,
-      parent: fiber,
-    };
-
-    if (index === 0) {
-      fiber.child = currentFiber;
-    } else {
-      prevSibling.sibling = currentFiber;
-    }
-    prevSibling = currentFiber;
-  });
+  reconceChildren(fiber, children);
 
   // return next unit of work
   if (fiber.child) {
@@ -94,6 +129,55 @@ function performUnitOfWork(fiber) {
       return nextFiber.sibling;
     }
     nextFiber = nextFiber.parent;
+  }
+}
+
+function reconceChildren(fiber, elements) {
+  let index = 0;
+  let oldFiber = fiber.alternate && fiber.alternate.child;
+  let prevSibling = null;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    const sameType = oldFiber && element && oldFiber.type === element.type;
+    let newFiber = null;
+
+    if (sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: fiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: fiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else if (element) {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
   }
 }
 
@@ -116,14 +200,19 @@ const Doact = {
 };
 
 /** @jsx Doact.createElement */
-const element = (
-  <div id="foo">
-    <h1>
-      <span>Hello world</span>
-      <span>^.^</span>
-    </h1>
-    <div style="width:100px;height:100px;border:1px solid;"></div>
-  </div>
-);
 const container = document.getElementById("root");
-Doact.render(element, container);
+
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
+
+const rerender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  );
+  Doact.render(element, container);
+};
+rerender("World");
